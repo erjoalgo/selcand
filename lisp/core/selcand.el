b59a4e2ef8755f857f3efcd12ce6c5eb820cc1af
Ernesto Alfonso <erjoalgo@gmail.com>
2019-01-24 00:22:17 -0800

    flycheck

--- lisp/core/selcand.el
+++ lisp/core/selcand.el
@@ -1,3 +1,51 @@
+;;; selcand.el --- Select a candidate from a tree of hint characters
+;;
+;; Filename: selcand.el
+;; Description:
+;; Author: Ernesto Alfonso
+;; Maintainer: (concat "erjoalgo" "@" "gmail" ".com")
+;; Created: Thu Jan 24 00:18:56 2019 (-0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 0
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
 (defcustom selcand-default-hints
   "qwertasdfzxcv1234"
   "Default hint chars."
@@ -9,7 +57,7 @@
 
   each hint consists of characters in the string CHARS."
   (setf chars (or chars selcand-default-hints))
-  (assert cands)
+  (cl-assert cands)
   (let* ((w (ceiling (log (length cands) (length chars))))
          (hints (cl-loop with curr = '("")
                          for wi below w do
@@ -34,8 +82,11 @@
          (choice (completing-read prompt choices
                                   nil
                                   t))
-         (cand (let* ((hint (car (s-split sep choice))))
+         (cand (let* ((hint (car (split-string choice sep))))
                  (cdr (assoc hint hints-cands #'equal)))))
     cand))
 
 (provide 'selcand)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; selcand.el ends here
