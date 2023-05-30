b86d50bfd274b7f4b8b0d2359a62723821966db9
Ernesto Alfonso <erjoalgo@gmail.com>
2023-05-30 17:13:39 -0400

    add usage note in commentary

--- selcand.el
+++ selcand.el
@@ -30,6 +30,8 @@
 ;; Like vimium, selcand-select enumerates a potentially large list
 ;; of candidates using very short hints that the user may type to
 ;; make a selection.
+;; It is intended to be used from emacs lisp code to prompt the user
+;; to select from a list of choices.
 ;; Sample usage: (switch-to-buffer (selcand-select (buffer-list)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
